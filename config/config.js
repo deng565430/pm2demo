2aaa08be5658b6ae86ae380b9550c91e27eddd98
deng565430 <562340148@qq.com>
2017-12-08 10:30:35 +0800

    修改端口号

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const CONFIG = new Map();
 
-CONFIG.set('prot', 3000);
+CONFIG.set('prot', 80);
 CONFIG.set('staticDir', path.join(__dirname, '..', 'static'));
 CONFIG.set('viewDir', path.join(__dirname, '..', 'server/views'));
 
