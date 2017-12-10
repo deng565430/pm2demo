27e2fc7e86b752fb72a2cea3e5c61a2f5fbd0254
deng565430 <562340148@qq.com>
2017-12-10 22:54:31 +0800

    增加shell

--- shell.js
+++ shell.js
@@ -2,4 +2,7 @@ const shell = require('shelljs');
 shell.exec('git pull');
 shell.exec('git add --all');
 shell.exec('git commit -m "增加shell"');
-shell.exec('git push');
+shell.exec('git push origin master');
+// 自动登录服务器
+shell.exec('ssh -i dengshenghui_sea root@114.67.231.71');
+shell.exec('scp /Users/mac/Desktop/myproject/2017pro/pm2demo/server/views/index.ejs root@114.67.231.71:/root/nodedemo/pm2demo/server/views')
